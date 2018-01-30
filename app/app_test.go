0b590bcffc708587838e33d3d64b229918564be6
yukai tu <tuyukai1994@gmail.com>
2018-01-30 11:52:10 -0800

    Add test case to lino application

---
+++ app/app_test.go
@@ -0,0 +1,40 @@
+package app
+
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/bank"
+	"github.com/stretchr/testify/assert"
+	crypto "github.com/tendermint/go-crypto"
+)
+
+func TestSendMsg(t *testing.T) {
+	tla := newTestLinocoinApp()
+	tla.RunBeginBlock()
+
+	// Construct a SendMsg.
+	var msg = bank.SendMsg{
+		Inputs: []bank.Input{
+			{
+				Address:  crypto.Address([]byte("input")),
+				Coins:    sdk.Coins{{"atom", 10}},
+				Sequence: 1,
+			},
+		},
+		Outputs: []bank.Output{
+			{
+				Address: crypto.Address([]byte("output")),
+				Coins:   sdk.Coins{{"atom", 10}},
+			},
+		},
+	}
+
+	// Run a Check on SendMsg.
+	res := tla.RunCheckMsg(msg)
+	assert.Equal(t, sdk.CodeOK, res.Code, res.Log)
+
+	// Run a Deliver on SendMsg.
+	res = tla.RunDeliverMsg(msg)
+	assert.Equal(t, sdk.CodeUnrecognizedAddress, res.Code, res.Log)
+}
