b51a41f69db42305a5e324b6baeaefe283d9ebbe
Yukai Tu <ytu@lino.network>
2018-06-11 16:37:11 -0700

    LNO-45, change from and to type to account key

--- x/account/handler.go
+++ x/account/handler.go
@@ -80,13 +80,13 @@ func handleTransferMsg(ctx sdk.Context, am AccountManager, msg TransferMsg) sdk.
 		return err.Result()
 	}
 	if err := am.MinusSavingCoin(
-		ctx, msg.Sender, coin, string(msg.Receiver), msg.Memo, types.TransferOut); err != nil {
+		ctx, msg.Sender, coin, msg.Receiver, msg.Memo, types.TransferOut); err != nil {
 		return err.Result()
 	}
 
 	// send coins using username
 	if err := am.AddSavingCoin(
-		ctx, msg.Receiver, coin, string(msg.Sender), msg.Memo, types.TransferIn); err != nil {
+		ctx, msg.Receiver, coin, msg.Sender, msg.Memo, types.TransferIn); err != nil {
 		return ErrTransferHandler(msg.Sender).TraceCause(err, "").Result()
 	}
 	return sdk.Result{}
@@ -123,7 +123,7 @@ func handleRegisterMsg(ctx sdk.Context, am AccountManager, msg RegisterMsg) sdk.
 		return err.Result()
 	}
 	if err := am.MinusSavingCoin(
-		ctx, msg.Referrer, coin, string(msg.NewUser), "", types.TransferOut); err != nil {
+		ctx, msg.Referrer, coin, msg.NewUser, "", types.TransferOut); err != nil {
 		return err.Result()
 	}
 	if err := am.CreateAccount(
