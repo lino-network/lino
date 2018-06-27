df22e33b8adef5cbf5ff3a313a8484e1ad49c1c8
Yukai Tu <ytu@lino.network>
2018-06-26 21:24:56 -0700

    LNO-68, remove useless block height event list

--- types/event.go
+++ types/event.go
@@ -2,11 +2,6 @@ package types
 
 type Event interface{}
 
-// Height -> HeightEventList
-type HeightEventList struct {
-	Events []Event `json:"events"`
-}
-
 // Minute -> TimeEventList
 type TimeEventList struct {
 	Events []Event `json:"events"`
