2fa5ba108e2c7cd5ce1f7fcb3f0709c14ab55889
Yukai Tu <ytu@lino.network>
2018-08-09 17:23:55 -0700

    LNO-122, deleted post can't get inflation

--- x/post/event_test.go
+++ x/post/event_test.go
@@ -19,8 +19,11 @@ func TestRewardEvent(t *testing.T) {
 	as := accModel.NewAccountStorage(TestAccountKVStoreKey)
 
 	user, postID := createTestPost(t, ctx, "user", "postID", am, pm, "0")
+	user2, deletedPostID := createTestPost(t, ctx, "user2", "deleted", am, pm, "0")
+	err := pm.DeletePost(ctx, types.GetPermlink(user2, deletedPostID))
+	assert.Nil(t, err)
 	user1 := createTestAccount(t, ctx, am, "user1")
-	err := dm.RegisterDeveloper(ctx, "LinoApp1", types.NewCoinFromInt64(1000000*types.Decimals), "", "", "")
+	err = dm.RegisterDeveloper(ctx, "LinoApp1", types.NewCoinFromInt64(1000000*types.Decimals), "", "", "")
 	assert.Nil(t, err)
 	err = dm.RegisterDeveloper(ctx, "LinoApp2", types.NewCoinFromInt64(1000000*types.Decimals), "", "", "")
 	assert.Nil(t, err)
@@ -257,6 +260,38 @@ func TestRewardEvent(t *testing.T) {
 				UnclaimReward:   types.NewCoinFromInt64(2075),
 			},
 		},
+		{
+			testName: "deleted post can't get any inflation",
+			rewardEvent: RewardEvent{
+				PostAuthor: user2,
+				PostID:     deletedPostID,
+				Consumer:   user1,
+				Evaluate:   types.NewCoinFromInt64(33333),
+				Original:   types.NewCoinFromInt64(100),
+				Friction:   types.NewCoinFromInt64(15),
+				FromApp:    types.AccountKey("LinoApp2"),
+			},
+			totalReportOfThePost: types.NewCoinFromInt64(0),
+			totalUpvoteOfThePost: types.NewCoinFromBigInt(bigStringInt2),
+			initRewardPool:       types.NewCoinFromInt64(5555),
+			initRewardWindow:     types.NewCoinFromInt64(77777),
+			expectPostMeta: postModel.PostMeta{
+				TotalUpvoteStake:        types.NewCoinFromBigInt(bigStringInt2),
+				TotalReportStake:        types.NewCoinFromInt64(0),
+				TotalDonateCount:        1,
+				TotalReward:             types.NewCoinFromInt64(0),
+				RedistributionSplitRate: sdk.ZeroRat(),
+				IsDeleted:               true,
+			},
+			expectAppWeight: sdk.NewRat(2175, 2326),
+			expectAuthorReward: accModel.Reward{
+				TotalIncome:     types.NewCoinFromInt64(0),
+				OriginalIncome:  types.NewCoinFromInt64(15),
+				FrictionIncome:  types.NewCoinFromInt64(15),
+				InflationIncome: types.NewCoinFromInt64(0),
+				UnclaimReward:   types.NewCoinFromInt64(0),
+			},
+		},
 	}
 
 	for _, tc := range testCases {
@@ -264,20 +299,21 @@ func TestRewardEvent(t *testing.T) {
 			ConsumptionRewardPool: tc.initRewardPool,
 			ConsumptionWindow:     tc.initRewardWindow,
 		})
-		pm.postStorage.SetPostMeta(ctx, types.GetPermlink(user, postID),
+		pm.postStorage.SetPostMeta(ctx, types.GetPermlink(tc.rewardEvent.PostAuthor, tc.rewardEvent.PostID),
 			&postModel.PostMeta{
 				TotalUpvoteStake: tc.totalUpvoteOfThePost,
 				TotalReportStake: tc.totalReportOfThePost,
 				TotalReward:      types.NewCoinFromInt64(0),
+				IsDeleted:        tc.expectPostMeta.IsDeleted,
 			})
 
-		as.SetReward(ctx, user, &accModel.Reward{})
+		as.SetReward(ctx, tc.rewardEvent.PostAuthor, &accModel.Reward{})
 
 		err := tc.rewardEvent.Execute(ctx, pm, am, gm, dm)
 		if err != nil {
 			t.Errorf("%s: failed to execute, got err %v", tc.testName, err)
 		}
-		checkPostMeta(t, ctx, types.GetPermlink(user, postID), tc.expectPostMeta)
+		checkPostMeta(t, ctx, types.GetPermlink(tc.rewardEvent.PostAuthor, tc.rewardEvent.PostID), tc.expectPostMeta)
 		if dm.DoesDeveloperExist(ctx, tc.rewardEvent.FromApp) {
 			consumptionWeight, err := dm.GetConsumptionWeight(ctx, tc.rewardEvent.FromApp)
 			if err != nil {
@@ -287,7 +323,7 @@ func TestRewardEvent(t *testing.T) {
 				t.Errorf("%s: diff consumption weight, got %v, want %v", tc.testName, consumptionWeight, tc.expectAppWeight)
 			}
 		}
-		reward, err := as.GetReward(ctx, user)
+		reward, err := as.GetReward(ctx, tc.rewardEvent.PostAuthor)
 		if err != nil {
 			t.Errorf("%s: failed to get reward, got err %v", tc.testName, err)
 		}
