09518b0b6b5a2aed2185b500ab3cc685c10a839f
zhimao liu <zhimao_liu@icloud.com>
2018-08-25 12:39:04 -0700

    add cmd support

--- x/proposal/commands/query.go
+++ x/proposal/commands/query.go
@@ -9,33 +9,33 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/wire"
 	"github.com/lino-network/lino/client"
-	"github.com/lino-network/lino/x/proposal/model"
 	"github.com/lino-network/lino/types"
+	"github.com/lino-network/lino/x/proposal/model"
 )
 
-// GetProposalCmd returns a specific proposal
-func GetProposalCmd(storeName string, cdc *wire.Codec) *cobra.Command {
+// GetProposalCmd returns a specific ongoing proposal
+func GetOngoingProposalCmd(storeName string, cdc *wire.Codec) *cobra.Command {
 	cmdr := commander{
 		storeName,
 		cdc,
 	}
 	return &cobra.Command{
-		Use:   "query-proposal",
-		Short: "Query a specific proposal",
-		RunE:  cmdr.getProposalCmd,
+		Use:   "query-ongoing-proposal",
+		Short: "Query a specific ongoing proposal",
+		RunE:  cmdr.getOngoingProposalCmd,
 	}
 }
 
-// GetProposalListCmd returns the proposal list
-func GetProposalListCmd(storeName string, cdc *wire.Codec) *cobra.Command {
+// GetProposalCmd returns a specific expired proposal
+func GetExpiredProposalCmd(storeName string, cdc *wire.Codec) *cobra.Command {
 	cmdr := commander{
 		storeName,
 		cdc,
 	}
 	return &cobra.Command{
-		Use:   "proposal-list",
-		Short: "Query ongoing and past proposal",
-		RunE:  cmdr.getProposalListCmd,
+		Use:   "query-expired-proposal",
+		Short: "Query a specific expired proposal",
+		RunE:  cmdr.getExpiredProposalCmd,
 	}
 }
 
@@ -44,7 +44,7 @@ type commander struct {
 	cdc       *wire.Codec
 }
 
-func (c commander) getProposalCmd(cmd *cobra.Command, args []string) error {
+func (c commander) getOngoingProposalCmd(cmd *cobra.Command, args []string) error {
 	ctx := client.NewCoreContextFromViper()
 	if len(args) != 1 {
 		return errors.New("You must provide proposal ID")
@@ -52,7 +52,7 @@ func (c commander) getProposalCmd(cmd *cobra.Command, args []string) error {
 
 	proposalID := types.ProposalKey(args[0])
 
-	res, err := ctx.Query(model.GetProposalKey(proposalID), c.storeName)
+	res, err := ctx.Query(model.GetOngoingProposalKey(proposalID), c.storeName)
 	if err != nil {
 		return err
 	}
@@ -70,24 +70,28 @@ func (c commander) getProposalCmd(cmd *cobra.Command, args []string) error {
 	return nil
 }
 
-func (c commander) getProposalListCmd(cmd *cobra.Command, args []string) error {
+func (c commander) getExpiredProposalCmd(cmd *cobra.Command, args []string) error {
 	ctx := client.NewCoreContextFromViper()
-	res, err := ctx.Query(model.GetProposalListKey(), c.storeName)
+	if len(args) != 1 {
+		return errors.New("You must provide proposal ID")
+	}
+
+	proposalID := types.ProposalKey(args[0])
+
+	res, err := ctx.Query(model.GetExpiredProposalKey(proposalID), c.storeName)
 	if err != nil {
 		return err
 	}
-
-	proposalList := new(model.ProposalList)
-	if err := c.cdc.UnmarshalJSON(res, proposalList); err != nil {
+	proposal := new(model.Proposal)
+	if err := c.cdc.UnmarshalJSON(res, proposal); err != nil {
 		return err
 	}
 
-	// print out whole bank
-	output, err := json.MarshalIndent(proposalList, "", "  ")
+	// print out whole vote
+	output, err := json.MarshalIndent(proposal, "", "  ")
 	if err != nil {
 		return err
 	}
 	fmt.Println(string(output))
-
 	return nil
 }
