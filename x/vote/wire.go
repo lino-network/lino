6a97d39407d9cccb67663a0eb22f50fcf9d1f020
zhimao liu <zhimao_liu@icloud.com>
2018-09-11 16:23:32 -0700

    fix type

--- x/vote/wire.go
+++ x/vote/wire.go
@@ -6,9 +6,9 @@ import (
 
 // Register concrete types on wire codec
 func RegisterWire(cdc *wire.Codec) {
-	cdc.RegisterConcrete(StakeInMsg{}, "lino/voteDeposit", nil)
-	cdc.RegisterConcrete(RevokeStakeMsg{}, "lino/voteRevoke", nil)
-	cdc.RegisterConcrete(StakeOutMsg{}, "lino/voteWithdraw", nil)
+	cdc.RegisterConcrete(StakeInMsg{}, "lino/stakeIn", nil)
+	cdc.RegisterConcrete(RevokeStakeMsg{}, "lino/revokeStake", nil)
+	cdc.RegisterConcrete(StakeOutMsg{}, "lino/stakeOut", nil)
 	cdc.RegisterConcrete(DelegateMsg{}, "lino/delegate", nil)
 	cdc.RegisterConcrete(DelegatorWithdrawMsg{}, "lino/delegateWithdraw", nil)
 	cdc.RegisterConcrete(RevokeDelegationMsg{}, "lino/delegateRevoke", nil)
