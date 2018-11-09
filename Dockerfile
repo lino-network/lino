2b8e48a701cf00c800b6dc3d01de042c3bec97eb
Yukai Tu <ytu@lino.network>
2018-11-09 15:10:22 -0800

    LNO-161, update docker file

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,21 @@ FROM golang:1.10
 
 RUN apt-get update && \
     apt-get -y install wget curl sudo make && \
-    apt-get install -y git
+    apt-get install -y git && \
+    apt-get install -y jq && \
+    apt install -y python3 \
+        python3-pip \
+        python3-setuptools && \
+        pip3 install --upgrade pip && \
+        apt-get clean
 
 # get lino core code
+RUN pip install awscli --upgrade --user
 RUN mkdir -p src/github.com/lino-network
 WORKDIR src/github.com/lino-network
 RUN git clone https://github.com/lino-network/lino.git
 WORKDIR lino
+RUN git checkout v0.1.5
 
 # golang dep
 RUN go get -u github.com/golang/dep/cmd/dep
@@ -23,6 +31,9 @@ RUN ./lino init
 
 COPY docker/fullnode/genesis.json /root/.lino/config/genesis.json
 COPY docker/fullnode/config.toml ./config.toml
+
+RUN sed -i "11s/.*/moniker=\"$(openssl rand -base64 6)\"/" config.toml
+RUN mv config.toml /root/.lino/config/config.toml
 
 EXPOSE 26656
 EXPOSE 26657
--- docker/fullnode/watch_dog.sh
+++ docker/fullnode/watch_dog.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
-node=$1
-sed -i "11s/.*/moniker=\"$node\"/" config.toml
-mv config.toml ~/.lino/config/
+/root/.local/bin/aws s3 cp s3://lino-blockchain-data/data.tar.gz data.tar.gz
+RUN rm -rf /root/.lino/data
+mkdir -p /root/.lino/data/
+tar -xzvf data.tar.gz -C /root/.lino/data/
+rm -rf data.tar.gz
 ./lino start --log_level=error &
 pid=$!
-echo pid is $pid
+last_height=0
 while true
  do
     sleep 30s
-    status=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:26657)
+    status=$(curl --max-time 10 -s -o /dev/null -w "%{http_code}" http://localhost:26657)
+    height=$(curl --max-time 10 http://localhost:26657/status | jq '. | .result.sync_info.latest_block_height')
+    echo "running at height $height"
     if [ $status -eq 200 ]
     then
       echo node is running
+      if [ "$height" = "$last_height" ]
+      then
+        echo node is at the same height about 30s
+        kill -9 $pid
+        sleep 10s
+        ./lino start --log_level=error &
+        pid=$!
+        healthy=false
+      else
+        echo node is healthy
+        last_height=$height
+      fi
     else
       echo node is down
-      curl -s -d "to=%2B13105629182" -d "body=node: $node is down!" https://utils.lib.id/sms@1.0.4/
-      curl -s -d "to=%2B18588595067" -d "body=node: $node is down!" https://utils.lib.id/sms@1.0.4/
-      curl -s -d "to=%2B13106941760" -d "body=node: $node is down!" https://utils.lib.id/sms@1.0.4/
-      curl -s -d "to=%2B19178737414" -d "body=node: $node is down!" https://utils.lib.id/sms@1.0.4/
       kill -9 $pid
-      ./lino start &
-      let pid=$!
+      sleep 10s
+      ./lino start --log_level=error &
+      pid=$!
+      healthy=false
     fi
 done
