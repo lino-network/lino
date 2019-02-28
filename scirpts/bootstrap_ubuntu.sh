89ae338c2740c564a995224da89baa53be9f8ec6
Yumin Xia <me@yumin-xia.com>
2019-02-27 19:52:11 -0800

    contd fixes

--- scirpts/bootstrap_ubuntu.sh
+++ scirpts/bootstrap_ubuntu.sh
@@ -8,17 +8,18 @@ GOPATH=\$HOME/go
 PROFILE=/home/ubuntu/.profile
 
 curl -sSL $GOSOURCE -o /tmp/go.tar.gz
-tar -xzf /tmp/go.tar.gz -C $GOTARGET
-rm /tmp/go.tar.gz
+sudo tar -xzf /tmp/go.tar.gz -C $GOTARGET
+sudo rm /tmp/go.tar.gz
 
 # apply environment configuration to the user's .profile
+touch $PROFILE
 printf "\n" >> $PROFILE
 printf "# golang configuration\n" >> $PROFILE
 printf "export GOROOT=$GOTARGET/go\n" >> $PROFILE
 printf "export GOPATH=$GOPATH\n" >> $PROFILE
 printf "export PATH=\$PATH:$GOTARGET/go/bin:$GOPATH/bin\n" >> $PROFILE
 
-source .profile
+source $PROFILE
 sudo ldconfig
 go version
 
