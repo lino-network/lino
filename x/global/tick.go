86246b71f27cd752e08261e408c6a2c9373e8b85
Yumin Xia <stumblecra@gmail.com>
2019-03-12 16:15:05 -0700

    adjust reward pool (#336)

--- x/global/tick.go
+++ x/global/tick.go
@@ -3,6 +3,8 @@ package global
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	abci "github.com/tendermint/tendermint/abci/types"
+
+	"github.com/lino-network/lino/types"
 )
 
 // BeginBlocker - called every begin blocker, udpate transaction per second
@@ -17,10 +19,26 @@ func BeginBlocker(
 	return
 }
 
+// EndBlocker - related to upgrade1update3.
 func EndBlocker(
 	ctx sdk.Context, req abci.RequestEndBlock, gm *GlobalManager) (tags sdk.Tags) {
 	if err := gm.CommitEventCache(ctx); err != nil {
 		panic(err)
 	}
+
+	// one time execution for upgrade1update3, divided by 6 because it's the approximate right
+	// reward pool amount.
+	if ctx.BlockHeight() == types.BlockchainUpgrade1Update3Height {
+		consumptionMeta, err := gm.storage.GetConsumptionMeta(ctx)
+		if err != nil {
+			panic(err)
+		}
+		consumptionMeta.ConsumptionRewardPool = types.DecToCoin(
+			consumptionMeta.ConsumptionRewardPool.ToDec().Quo(sdk.NewDec(6)))
+		if err := gm.storage.SetConsumptionMeta(ctx, consumptionMeta); err != nil {
+			panic(err)
+		}
+	}
+
 	return
 }
