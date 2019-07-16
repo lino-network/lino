6161d872f0024a307a8442ff99f051b474bca4a2
Yumin Xia <me@yumin-xia.com>
2019-07-16 16:34:30 -0700

    install zstd from source

--- scripts/install_rocksdb.sh
+++ scripts/install_rocksdb.sh
@@ -3,8 +3,17 @@
 sudo apt-get update
 sudo apt-get install -y build-essential gcc g++ make
 
-sudo apt-get install -y libgflags-dev libzstd-dev libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev
+sudo apt-get install -y libgflags-dev libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev
+
+# ubuntu 16.04's zstd-dev version is too low.
+sudo apt-get remove -y libzstd-dev
+
 homedir="$PWD"
+wget -O zstd.tar.gz https://github.com/facebook/zstd/archive/v1.4.0.tar.gz && \
+    tar -zxvf zstd.tar.gz && \
+    cd zstd-1.4.0/ && \
+    sudo make install -j4
+cd "$homedir"
 
 wget -O rocks.tar.gz https://github.com/facebook/rocksdb/archive/v6.1.2.tar.gz && \
   tar -zxvf rocks.tar.gz && \
@@ -19,3 +28,5 @@ cd "$homedir"
 
 rm rocks.tar.gz
 rm -rf rocksdb-6.1.2
+rm zstd.tar.gz
+rm -rf zstd-1.4.0
