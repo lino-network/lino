d7ff8fc75f37251002f90b28df5336cc6baf0054
Yumin Xia <me@yumin-xia.com>
2019-07-16 17:10:27 -0700

    fix compile error on old platform

--- scripts/install_rocksdb.sh
+++ scripts/install_rocksdb.sh
@@ -3,16 +3,17 @@
 sudo apt-get update
 sudo apt-get install -y build-essential gcc g++ make
 
-sudo apt-get install -y libgflags-dev libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev
+sudo apt-get install -y libgflags-dev
 
 # ubuntu 16.04's zstd-dev version is too low.
-sudo apt-get remove -y libzstd-dev
+sudo apt-get remove -y libzstd-dev libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev
 
 homedir="$PWD"
 wget -O zstd.tar.gz https://github.com/facebook/zstd/archive/v1.4.0.tar.gz && \
     tar -zxvf zstd.tar.gz && \
     cd zstd-1.4.0/ && \
-    sudo make install -j4
+    sudo make install -j4 && \
+    sudo ldconfig
 cd "$homedir"
 
 wget -O rocks.tar.gz https://github.com/facebook/rocksdb/archive/v6.1.2.tar.gz && \
@@ -21,6 +22,7 @@ wget -O rocks.tar.gz https://github.com/facebook/rocksdb/archive/v6.1.2.tar.gz &
   make static_lib -j4 && \
   sudo cp librocksdb.a /usr/local/lib/ && \
   cd include/ && \
+  sudo rm -rf /usr/local/include/rocksdb && \
   sudo cp -r rocksdb /usr/local/include/ && \
   sudo ldconfig
 
