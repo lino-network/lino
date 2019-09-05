fb2379d555d808fb600127bff901e49fed442ce4
Yumin Xia <me@yumin-xia.com>
2019-09-05 12:26:20 -0700

    update scripts

--- scripts/install_rocksdb.sh
+++ scripts/install_rocksdb.sh
@@ -6,16 +6,16 @@ sudo apt-get install -y build-essential gcc g++ make
 sudo apt-get install -y libgflags-dev
 
 homedir="$PWD"
-wget -O zstd.tar.gz https://github.com/facebook/zstd/archive/v1.4.0.tar.gz && \
+wget -O zstd.tar.gz https://github.com/facebook/zstd/archive/v1.4.3.tar.gz && \
     tar -zxvf zstd.tar.gz && \
-    cd zstd-1.4.0/ && \
+    cd zstd-1.4.3/ && \
     sudo make install -j4 && \
     sudo ldconfig
 cd "$homedir"
 
-wget -O rocks.tar.gz https://github.com/facebook/rocksdb/archive/v6.1.2.tar.gz && \
+wget -O rocks.tar.gz https://github.com/facebook/rocksdb/archive/v6.2.2.tar.gz && \
   tar -zxvf rocks.tar.gz && \
-  cd rocksdb-6.1.2/ && \
+  cd rocksdb-6.2.2/ && \
   make static_lib -j4 && \
   sudo cp librocksdb.a /usr/local/lib/ && \
   cd include/ && \
@@ -26,6 +26,6 @@ wget -O rocks.tar.gz https://github.com/facebook/rocksdb/archive/v6.1.2.tar.gz &
 cd "$homedir"
 
 rm rocks.tar.gz
-rm -rf rocksdb-6.1.2
+rm -rf rocksdb-6.2.2
 rm zstd.tar.gz
-rm -rf zstd-1.4.0
+rm -rf zstd-1.4.3
