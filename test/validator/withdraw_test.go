e95ccbd978182ada2b86ceb19fe9b80d8c5abfab
Yukai Tu <ytu@lino.network>
2019-09-25 13:09:42 -0700

    fix lint

--- test/validator/withdraw_test.go
+++ test/validator/withdraw_test.go
@@ -26,7 +26,7 @@ func TestValidatorRevoke(t *testing.T) {
 	test.CreateAccount(t, newAccountName, lb, 0,
 		newAccountResetPriv, newAccountTransactionPriv, newAccountAppPriv, "500000")
 
-	voteDepositMsg := types.NewStakeInMsg(newAccountName, types.LNO("300000"))
+	voteDepositMsg := types.NewStakeInMsg(newAccountName, linotypes.LNO("300000"))
 	test.SignCheckDeliver(t, lb, voteDepositMsg, 0, true, newAccountTransactionPriv, baseTime)
 
 	valDepositMsg := val.NewValidatorDepositMsg(
